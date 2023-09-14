38801e1eb5a1ec5b2c5fb418f9c39988728d0be5
Giulio Zelante <giulio.zelante@alkemy.com>
2023-09-14 15:09:53 +0200

    feat(build_docker_and_start.bat): add script to build Docker image, transfer to remote server, and start container The script `build_docker_and_start.bat` has been added. It includes the following steps: 1. Builds and cleans the Docker image using `gradlew clean optimizedDockerBuild`. 2. Saves the Docker image to a tar file named `tekken7-offline-tg-bot.tar`. 3. Transfers the tar file to a remote server at `root@192.168.1.105:/home/gzelante` using `scp`. 4. SSHs into the remote server. 5. Kills any existing Docker container named `t7_offline_tg_bot`. 6. Loads the Docker image from the tar file using `docker load -i /home/gzelante/tekken7-offline-tg-bot.tar`. 7. Runs a new Docker container named `t7_offline_tg_bot` with the specified environment variables and options.
    These changes were made to automate the process of building and deploying the Docker image for the Tekken 7 offline Telegram bot.

---
+++ build_docker_and_start.bat
@@ -0,0 +1,28 @@
+@echo off
+
+REM Build and clean Docker image
+call gradlew clean optimizedDockerBuild
+
+REM Save Docker image to tar file
+call docker save tekken7-offline-tg-bot:latest > tekken7-offline-tg-bot.tar
+
+REM Transfer tar file to remote server
+call scp tekken7-offline-tg-bot.tar root@192.168.1.105:/home/gzelante
+
+REM SSH into remote server
+call ssh root@192.168.1.105 "<< EOF
+
+#REM Kill existing Docker container
+docker kill t7_offline_tg_bot
+
+#REM Load Docker image from tar file
+docker load -i /home/gzelante/tekken7-offline-tg-bot.tar
+
+#REM Run Docker container with environment variables
+docker run -d -e T7_OFFLINE_BOT_DB_PASSWORD \
+                -e T7_OFFLINE_BOT_DB_USERNAME \
+                -e T7_OFFLINE_BOT_TOKEN \
+                -e T7_OFFLINE_BOT_USERNAME \
+                -e VIRUS_TOTAL_API_KEY \
+                --rm --name t7_offline_tg_bot -it tekken7-offline-tg-bot:latest
+EOF"
