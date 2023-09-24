6e4f918166a40174d3c7db975ff454c0ed260b7b
Giulio Zelante <giulio.zelante@alkemy.com>
2023-09-24 17:59:59 +0200

    feat: add volume mapping for log files in Docker container and update logback.xml configuration
    The build_docker_and_start.bat script now includes a volume mapping for the log files in the Docker container. This allows the logs to be persisted outside of the container in the specified directory "/var/logs/tekken-offline-tg-bot".
    
    The logback.xml configuration file has been updated to include a new appender named "FILE" which writes log messages to a file in the specified directory. The log file name is set to "log" and it will be rolled over daily with a maximum of 30 days' worth of history. The total size of the log files is capped at 3GB.
    
    These changes improve the logging capabilities of the application by ensuring that log files are persisted and can be accessed outside of the Docker container.

--- build_docker_and_start.bat
+++ build_docker_and_start.bat
@@ -30,6 +30,24 @@ if %errorlevel% neq 0 (
 REM Save Docker image to tar file
 call docker save tekken7-offline-tg-bot:latest > %DOCKER_IMAGE_TAR%
 
-REM Transfer tar file and docker-compose.yml to remote server
-REM call sshpass -p %SSH_PASSWORD% scp -P %SSH_PORT% %DOCKER_IMAGE_TAR% docker-compose.yml %SSH_USER%@%SSH_HOST%:%REMOTE_DIR%
-REM call sshpass -p %SSH_PASSWORD% ssh -p %SSH_PORT% %DOCKER_IMAGE_TAR% "/etc/local.d/start_t7_offline_bot.start"
+REM Transfer tar file to remote server
+call scp tekken7-offline-tg-bot.tar root@192.168.1.105:/home/gzelante
+
+REM SSH into remote server
+call ssh root@192.168.1.105 "<< EOF
+
+#REM Kill existing Docker container
+docker kill t7_offline_tg_bot
+
+#REM Load Docker image from tar file
+docker load -i /home/gzelante/tekken7-offline-tg-bot.tar
+
+#REM Run Docker container with environment variables
+docker run -d -e T7_OFFLINE_BOT_DB_PASSWORD \
+                -e T7_OFFLINE_BOT_DB_USERNAME \
+                -e T7_OFFLINE_BOT_TOKEN \
+                -e T7_OFFLINE_BOT_USERNAME \
+                -e VIRUS_TOTAL_API_KEY \
+                --rm --name t7_offline_tg_bot -it tekken7-offline-tg-bot:latest \
+                -v /var/logs/tekken-offline-tg-bot:/var/logs/tekken-offline-tg-bot
+EOF"
