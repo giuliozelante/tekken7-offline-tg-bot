725f63b35ee84491c6c1752f0a7d8dc9a464297f
Giulio Zelante <giulio.zelante@alkemy.com>
2023-10-14 18:22:08 +0200

    feat: add batch module for running deployment commands feat: add Micronaut CLI configuration file feat: add Batch class for running deployment commands feat: add application.properties file for batch module feat: add logback.xml configuration file for batch module feat: add BatchTest class for testing batch module refactor: update build.gradle for bot module to remove unnecessary dependencies and configurations refactor: update build.gradle for root project to remove unnecessary dependencies and configurations The changes include adding a new batch module that contains the Batch class for running deployment commands. The Micronaut CLI configuration file and application.properties file are added to the batch module. The logback.xml configuration file is also added for logging in the batch module. The BatchTest class is added for testing the batch module. The build.gradle files for the bot module and the root project are refactored to remove unnecessary dependencies and configurations. These changes are made to improve the structure and functionality of the project.

--- batch/src/main/java/it/giuliozelante/tekken7/offline/tg/bot/batch/Batch.java
+++ batch/src/main/java/it/giuliozelante/tekken7/offline/tg/bot/batch/Batch.java
@@ -1,80 +1,46 @@
 package it.giuliozelante.tekken7.offline.tg.bot.batch;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class Batch {
-    private static class BatchImpl implements Runnable {
-
-        public void run() {
-            List<String> commands = List.of(
-                    "gradlew clean optimizedDockerBuild",
-                    "docker save tekken7-offline-tg-bot:latest > tekken7-offline-tg-bot.tar",
-                    "scp tekken7-offline-tg-bot.tar root@192.168.1.105:/home/gzelante",
-                    "ssh root@192.168.1.105"
-            );
-
-            for (String command : commands) {
-                try {
-                    executeCommand(command);
-                } catch (IOException | InterruptedException e) {
-                    log.error(e.getMessage(), e);
-                }
-            }
+    public static void main(String args[]) {
+        Batch batch = new Batch();
+        String[] commands = List.of("gradlew clean optimizedDockerBuild",
+                "docker save tekken7-offline-tg-bot:latest > tekken7-offline-tg-bot.tar",
+                "scp tekken7-offline-tg-bot.tar root@192.168.1.105:/home/gzelante",
+                "ssh root@192.168.1.105").toArray(String[]::new);
+        try {
+            batch.runCommandsInSeries(commands);
+        } catch (InterruptedException | IOException e) {
+            log.error(e.getMessage(), e);
         }
 
-        private void executeCommand(String command) throws IOException, InterruptedException {
+    }
+
+    public void runCommandsInSeries(String[] commands) throws IOException, InterruptedException {
+        for (String command : commands) {
             String firstCommand;
             Path filePath = Paths.get(command.split(" ")[0]);
-            if (filePath.toAbsolutePath().toFile().exists())
+            if(filePath.toAbsolutePath().toFile().exists())
                 firstCommand = filePath.toAbsolutePath().toString();
             else
                 firstCommand = filePath.toString();
-            ProcessBuilder processBuilder = new ProcessBuilder(Stream
-                    .concat(Stream.of(firstCommand),
-                            Arrays.stream(Arrays.copyOfRange(command.split(" "), 1, command.split(" ").length)))
-                    .collect(Collectors.toList()));
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
-
-            // Connect the process streams to the current console
-            InputStream processInputStream = process.getInputStream();
-            OutputStream consoleOutputStream = System.out;
-            Thread inputThread = new Thread(() -> {
-                try {
-                    int c;
-                    while ((c = processInputStream.read()) != -1) {
-                        consoleOutputStream.write(c);
-                    }
-                } catch (IOException e) {
-                    log.error("Error reading process output", e);
-                }
-            });
-            inputThread.start();
-
-            // Wait for the process to complete
-            int exitCode = process.waitFor();
-            if (exitCode != 0) {
-                log.error("Command failed with exit code: " + exitCode);
-            }
-
-            // Wait for the input thread to finish
-            inputThread.join();
+            ProcessBuilder pb = new ProcessBuilder(Stream.concat(Stream.of(firstCommand), Arrays.stream(Arrays.copyOfRange(command.split(" "),1, command.split( " ").length))).collect(Collectors.toList()));
+            pb.redirectErrorStream(true);
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+            Process process = pb.start();
+            process.waitFor();
         }
-    }
-
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new BatchImpl());
-        t1.start();
     }
 }
