33bffb8d6509f8826e6ac0c7b9af90ae44219082
neo16 <giulio.zelante@gmail.com>
2024-09-26 02:19:02 +0200

    fix(application.yml): change default DB URL to localhost for local dev
    refactor(logback.xml): switch from file-based logging to console logging
    
    chore(build_docker_and_start.bat): combine scp commands for efficiency
    
    fix(docker-compose.yml): update H2 image and set network_mode to host

--- build_docker_and_start.bat
+++ build_docker_and_start.bat
@@ -17,8 +17,5 @@ if %errorlevel% neq 0 (
 REM Save Docker image to tar file
 docker save tekken7-offline-tg-bot:latest > tekken7-offline-tg-bot.tar
 
-REM Transfer tar file to remote server
-call scp tekken7-offline-tg-bot.tar root@192.168.1.105:/home/gzelante
-
-REM Transfer docker-compose.yml to remote server
-call scp docker-compose.yml root@192.168.1.105:/home/gzelante
+REM Transfer tar file and docker-compose.yml to remote server
+call scp tekken7-offline-tg-bot.tar docker-compose.yml root@192.168.1.105:/home/gzelante
