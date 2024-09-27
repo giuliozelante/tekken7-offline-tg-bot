df5cad207fcbbdfdbbe0e12031784b37e9eb9425
Giulio Zelante <giulio.zelante@gmail.com>
2024-09-27 17:52:59 +0200

    chore(build): update build scripts to check for changes in code before Docker image build, save, and transfer feat(build): add support for transferring Docker image and deploy script to remote server using sshpass feat(build): add support for executing deploy script on remote server using sshpass feat(build): update docker-compose.yml to define network configuration for services

--- build_docker_and_start.sh
+++ build_docker_and_start.sh
@@ -33,9 +33,9 @@ echo "Deployment completed. Check the remote server for the running container."
 #!/bin/bash
 
 # Load environment variables from .env file
-set -a
+set -o allexport
 source .env
-set +a
+set +o allexport
 
 # Build and clean Docker image
 ./gradlew clean optimizedDockerBuild
@@ -50,9 +50,9 @@ fi
 docker save bot:latest > "$DOCKER_IMAGE_TAR"
 
 # Transfer tar file and deploy script to remote server
-scp -P "$SSH_PORT" "$DOCKER_IMAGE_TAR" "$SSH_USER@$SSH_HOST:$REMOTE_DIR"
+sshpass -p "$SSH_PASSWORD" scp -P "$SSH_PORT" "$DOCKER_IMAGE_TAR" docker-compose.yml "$SSH_USER@$SSH_HOST:$REMOTE_DIR"
 
 # Execute the deploy script on the remote server
-ssh -p "$SSH_PORT" "$SSH_USER@$SSH_HOST" "/etc/local.d/start_t7_offline_bot.start"
+sshpass -p "$SSH_PASSWORD" ssh -p "$SSH_PORT" "$SSH_USER@$SSH_HOST" "/etc/local.d/start_t7_offline_bot.start"
 
 echo "Deployment completed. Check the remote server for the running container."
