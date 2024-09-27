936e61994364f806b4fdd5f4a1ad8ed7799b494f
Giulio Zelante <giulio.zelante@gmail.com>
2024-09-27 17:52:59 +0200

    chore(build): update build scripts to check for changes in code before Docker image build, save, and transfer feat(build): add support for transferring Docker image and deploy script to remote server using sshpass feat(build): add support for executing deploy script on remote server using sshpass feat(build): update docker-compose.yml to define network configuration for services

--- build_docker_and_start.bat
+++ build_docker_and_start.bat
@@ -1,5 +1,12 @@
 @echo off
 
+REM Check for changes in the code using git
+git diff --quiet HEAD
+if %errorlevel% equ 0 (
+    echo No changes detected. Skipping Docker image build, save, and transfer.
+    exit /b 0
+)
+
 REM Load environment variables from .env file
 for /f "tokens=*" %%a in (.env) do (
     set %%a
@@ -15,7 +22,9 @@ if %errorlevel% neq 0 (
 )
 
 REM Save Docker image to tar file
-docker save tekken7-offline-tg-bot:latest > tekken7-offline-tg-bot.tar
+docker save tekken7-offline-tg-bot:latest > %DOCKER_IMAGE_TAR%
 
 REM Transfer tar file and docker-compose.yml to remote server
-call scp tekken7-offline-tg-bot.tar docker-compose.yml root@$SSH_HOST:/home/gzelante
+call sshpass -p %SSH_PASSWORD% scp -P %SSH_PORT% %DOCKER_IMAGE_TAR% docker-compose.yml %SSH_USER%@%SSH_HOST%:%REMOTE_DIR%
+
+call shpass -p %SSH_PASSWORD% ssh -p %SSH_PORT% %DOCKER_IMAGE_TAR% "/etc/local.d/start_t7_offline_bot.start"
